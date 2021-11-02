d47ea60a2a9c2a7584e69b34f867218a5383eb99
nicoclos <n.bia@outlook.fr>
2021-11-02 03:52:28 +0100

    advanced

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,17 @@
+class IndexController < ApplicationController
+    def team
+                
+    end
+
+    def contact
+        
+    end
+
+    def home
+        @gossips = Gossip.all
+    end
+
+    def welcome
+        @first_name = params[:first_name]
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/team', to: 'index#team'
+  get '/contact', to: 'index#contact'
+  root 'index#home'
+  get '/welcome/:first_name', to: 'index#welcome'
+  get '/users/:id', to: 'users#show'
+  get 'gossips/:id', to: 'gossips#show' 
 end
